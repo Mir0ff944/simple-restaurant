283df76dc71a89d6fca335773291c2ff20194d24
Miroslav Ivanov <housera94@gmail.com>
2018-04-28 09:34:50 +0100

    Set default :bg locales

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Restaurant20
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Locales config
+    config.i18n.default_locale = :bg
   end
 end
