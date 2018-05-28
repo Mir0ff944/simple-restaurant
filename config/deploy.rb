799589b1311d5ed4a5df2437a94fec09745102b5
Miroslav Ivanov <housera94@gmail.com>
2018-05-28 18:58:28 +0100

    - add env.production.yml to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/deploy/restaurant"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-append :linked_files, "config/secrets.yml", 'config/api_keys.rb'
+append :linked_files, 'config/secrets.yml', 'config/api_keys.rb', 'config/env.production.yml'
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
