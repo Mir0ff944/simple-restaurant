d219f8c692b39735a7df6466ee8079d7ded2634f
Miroslav Ivanov <housera94@gmail.com>
2018-07-13 21:58:49 +0100

    - name correction

--- test/integration/simple_integration_test.rb
+++ test/integration/simple_integration_test.rb
@@ -2,12 +2,12 @@
 
 require 'test_helper'
 
-class HomeControllerTest < ActionDispatch::IntegrationTest
+class SimpleIntegrationTest < ActionDispatch::IntegrationTest
   test 'be able to visit home page' do
     get '/'
     assert_response :success
 
-    assert_select 'a.navbar-brand', 'Restaurant'
+    assert_select 'a.navbar-brand', 'Бистро Аги'
     assert_select 'a.nav-link', 'Меню'
     assert_select 'a.nav-link', 'Галерия'
     assert_select 'a.nav-link', 'Карта'
