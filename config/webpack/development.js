68b1c5a71e47893d19f244625e94b5f03aefe3da
Miroslav Ivanov <housera94@gmail.com>
2018-12-08 22:20:23 +0000

    - webpack change css loader and update entry point

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,3 +3,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: "./app/assets/javascripts/application.js"
+  }
+};
--- config/webpack/webpack.js
+++ config/webpack/webpack.js
@@ -1,3 +1,35 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: "../../app/views/layouts/application.html.erb"
-}
+  entry: "./app/assets/javascripts/application.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: "[local]___[hash:base64:5]"
+            }
+          },
+
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      }
+
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin()
+  ]
+};
