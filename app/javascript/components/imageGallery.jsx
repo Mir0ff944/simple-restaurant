1e4b13ab8506d98f7025930229bdeee3bfb8cd7b
Miroslav Ivanov <mivanov@scholastic.co.uk>
2019-04-05 23:11:07 +0100

    Add smaller derivatives for screens uder 991px

--- app/javascript/components/imageGallery.jsx
+++ app/javascript/components/imageGallery.jsx
@@ -18,31 +18,37 @@ import restaurant_4 from '../images/restaurant_4.jpg'
 import restaurant_5 from '../images/restaurant_5.jpg'
 import restaurant_5_small from '../images/restaurant_5_small.jpg'
 
-
-// convert to using setsrc http://neptunian.github.io/react-photo-gallery/examples/srcset-and-sizes.html
 const photos = [
   { src: `${dish_1}`, width: 4, height: 3, alt: 'Dishes on display' },
-  { src: `${dish_2}`, width: 4, height: 3, alt: 'Fries on oven and sausages'},
+  { src: `${dish_2}`, width: 4, height: 3, alt: 'Fries on oven and sausages' },
   { src: `${dish_3}`, width: 4, height: 2, alt: 'Musaka' },
-  { src: `${dish_4}`,
+  {
+    src: `${dish_4}`,
     srcSet: [`${dish_4} 1440w`, `${dish_4_small} 1024w`],
     size: ['(min-width 768px) 40vw', '(min-width 1024px) 33.3vw'],
-    width: 4, height: 3, alt: 'Dishes on display 2' },
-  { src: `${dish_5}`,
-    srcSet: [`${dish_5} 1440w`,`${dish_5_preview} 1024w`],
+    width: 4, height: 3, alt: 'Dishes on display 2'
+  },
+  {
+    src: `${dish_5}`,
+    srcSet: [`${dish_5} 1440w`, `${dish_5_preview} 1024w`],
     size: ['(min-width 768px) 40vw', '(min-width 1024px) 33.3vw'],
-    width: 4, height: 2, alt: 'Mackerel with lemon' },
-  { src: `${dish_6}`,
+    width: 4, height: 2, alt: 'Mackerel with lemon'
+  },
+  {
+    src: `${dish_6}`,
     srcSet: [`${dish_6} 1440w`, `${dish_6_small} 1024w`],
     size: ['(min-width 768px) 40vw', '(min-width 1024px) 33.3vw'],
-    width: 4, height: 2, alt: 'Chicken with potatoes and vegetables' },
+    width: 4, height: 2, alt: 'Chicken with potatoes and vegetables'
+  },
   { src: `${restaurant_1}`, width: 4, height: 3, alt: 'View 1' },
   { src: `${restaurant_2}`, width: 4, height: 3, alt: 'View 2' },
   { src: `${restaurant_4}`, width: 5, height: 6, alt: 'View 3' },
-  { src: `${restaurant_5}`,
+  {
+    src: `${restaurant_5}`,
     srcSet: [`${restaurant_5} 1440w`, `${restaurant_5_small} 1024w`],
     size: ['(min-width 768px) 40vw', '(min-width 1024px) 33.3vw'],
-    width: 4, height: 3, alt: 'View 4' }
+    width: 4, height: 3, alt: 'View 4'
+  }
 ];
 
 export default class ImageGallery extends React.Component {
