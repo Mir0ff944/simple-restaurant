9c37981c3fd8558702d4b304b12973301a60f57a
Miroslav Ivanov <mivanov@scholastic.co.uk>
2019-05-26 19:59:49 +0100

    Add second restaurant on Google Maps
    Adds a second marker on Google Maps with location for the second venue

--- app/assets/javascripts/custom_js/google_maps_api.js
+++ app/assets/javascripts/custom_js/google_maps_api.js
@@ -1,14 +1,27 @@
 function initMap() {
-  var map;
-  var location = { lat: 42.685238, lng: 23.280111 };
-
-  map = new google.maps.Map(document.getElementById('map'), {
+  var location = { lat: 42.683921, lng: 23.279224 };
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: location,
-    zoom: 17
+    zoom: 16.7
   });
 
-  var marker = new google.maps.Marker({
-    position: location,
-    map: map
-  });
+  setMarkers(map);
+}
+
+var markers = [
+  ['Karavana Agi', 42.682316, 23.278106, 2, 'B'],
+  ['Bistro Agi', 42.685193, 23.280122, 1, 'A']
+];
+
+function setMarkers(map) {
+  for (var i = 0; i < markers.length; i++) {
+    var venue = markers[i]
+    var marker = new google.maps.Marker({
+      position: {lat: venue[1], lng: venue[2]},
+      map: map,
+      title: venue[0],
+      zIndex: venue[3],
+      label: venue[4]
+    });
+  };
 }
